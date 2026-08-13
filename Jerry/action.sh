a80275aa8396a654dd1635b0fb73b9b3eb98de5b
WajahatNaeem056 <M.Wajahat0497@gmail.com>
2026-08-13 09:46:11 +0500

    Add missing shebangs to fix all SC2148 shellcheck errors

--- Jerry/action.sh
+++ Jerry/action.sh
@@ -1,3 +1,4 @@
+#!/system/bin/sh
 MODDIR=${0%/*}
 
 set +o standalone
--- Jerry/webroot/common/FixWidevineL1/FixWidevineL1.sh
+++ Jerry/webroot/common/FixWidevineL1/FixWidevineL1.sh
@@ -1,3 +1,4 @@
+#!/system/bin/sh
 _abi=$(getprop ro.product.cpu.abi 2>/dev/null)
 case "$_abi" in
   arm64|x86_64) _lib="/vendor/lib64/hw" ;;
