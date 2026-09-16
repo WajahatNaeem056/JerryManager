0f77d52847716862a3e566b2048d48fed72080ad
WajahatNaeem056 <m.wajahat0497@gmail.com>
2026-09-16 16:03:24 +0500

    Add shellcheck shell directive to sourced lib scripts

--- Jerry/lib/package_list.sh
+++ Jerry/lib/package_list.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=sh
 FIXED_TARGETS="\
 android
 com.android.vending
--- Jerry/lib/paths.sh
+++ Jerry/lib/paths.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=sh
 # Tricky Store paths
 TRICKY_DIR="/data/adb/tricky_store"
 TARGET_FILE="$TRICKY_DIR/keybox.xml"
